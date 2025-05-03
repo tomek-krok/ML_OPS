eb62fbf1ae455abb5c7b312949536b31e96ab526
Tomek Krok <tomek.krok@gmail.com>
2025-05-03 13:40:51 +0200

    set validation for environment var.

--- settings.py
+++ settings.py
@@ -1,18 +1,17 @@
 from pydantic_settings import BaseSettings
-from pydantic import validator
+from pydantic import field_validator, ValidationError
 
 
 class Settings(BaseSettings):
     ENVIRONMENT: str
     APP_NAME: str
 
-    @validator("environment")
-    def validate_environment(cls, value):
-        # implement me!
-        # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+    @field_validator("ENVIRONMENT")
+    def validate_environment(value):
+        #     # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+        env_variables = ["dev", "test", "prod"]
 
-        value = "ZERO"
-
-        # add implementation here !!! now we will jump to lab 02
-
-        return value
+        if value in env_variables:
+            return value
+        else:
+            raise ValidationError()
