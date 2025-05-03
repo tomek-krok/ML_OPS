cd7c9299a64eb7113a9c969f150efd415c3e6ae2
Tomek Krok <tomek.krok@gmail.com>
2025-05-03 14:34:02 +0200

    added main context with export _envs

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from ml-ops!")
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+
+
+def export_envs(environment: str = "dev") -> None:
+    exist = load_dotenv(".env." + environment)
+
+    print(f"my env exist is {exist}")
+
+    if not exist:
+        raise Exception(f"Environment file .env.{environment} does not exist")
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
